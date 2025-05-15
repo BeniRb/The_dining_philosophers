34995fe47e4d6a7d36582e7a952de0b744877a30
BeniRb <beni.ribakov@gmail.com>
2025-05-15 10:54:35 +0300

    fixed a small bug

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        Fork fork1 = new Fork(1);
+        Fork fork2 = new Fork(2);
+        Fork fork3 = new Fork(3);
+        Fork fork4 = new Fork(4);
+        Fork fork5 = new Fork(5);
+        Philosopher philosopher1 = new Philosopher("John Locke", fork1, fork2);
+        Philosopher philosopher2 = new Philosopher("Plato", fork2, fork3);
+        Philosopher philosopher3 = new Philosopher("Freud", fork3, fork4);
+        Philosopher philosopher4 = new Philosopher(" Marx", fork4, fork5);
+        Philosopher philosopher5 = new Philosopher("Nietzsche", fork5, fork1);
+        new Thread(() -> {
+            while (true) {
+                System.out.println("-------------------------------------------------");
+                System.out.println(philosopher1);
+                System.out.println(philosopher2);
+                System.out.println(philosopher3);
+                System.out.println(philosopher4);
+                System.out.println(philosopher5);
+                Utils.sleep(3000);
+            }
+        }).start();
+    }
+}
